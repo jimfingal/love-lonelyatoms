9f6fc5a7590e860d91ad3fb77ffc2ae703391239
jimmytheleaf <jim.fingal@gmail.com>
2013-09-23 00:47:00 -0700

    remove duplicate code

--- src/games/juicy/scripts/oldeffects.lua
+++ src/games/juicy/scripts/oldeffects.lua
@@ -118,26 +118,6 @@ function EffectDispatcher:dropInPlayer()
 end
 
 
-function EffectDispatcher.cameraShake()
-        
-    local camera = world:getSystem(CameraSystem)
-    local schedule_system = world:getSystem(ScheduleSystem)
-    local tween_system = world:getSystem(TweenSystem)
-
-    intensity = 3
-
-    local jitter = function()
-        camera.transform:move(math.random(-intensity, intensity), math.random(-intensity, intensity))
-    end
-
-    schedule_system:doFor(0.3, 
-        jitter,
-        function()
-            tween_system:addTween(0.1, camera.transform:getPosition(), {x = 0, y = 0}, Easing.linear)
-        end
-    )
-
-end
 
 function EffectDispatcher.scaleEntity(entity, dx, dy)
 
@@ -201,16 +181,6 @@ function EffectDispatcher.allEffects(entity, dx, dy)
     EffectDispatcher.rotateEntity(entity) 
 end
 
-
-function EffectDispatcher.slowMo(length)
-
-    local time_system = world:getSystem(TimeSystem)
-
-    time_system.dilation = 0.1
-
-    world:getSystem(TweenSystem):addTween(length, time_system, {dilation = 1 }, Easing.outQuad)
-
-end
 
 
 function EffectDispatcher.dispatchBrick(ball, brick)
